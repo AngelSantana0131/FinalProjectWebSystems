458ada607a7f4e9d3df2fdd4078535ac1682dda6
Angel Santana <n01257615@unf.edu>
2021-04-15 15:55:20 -0400

    Created a temporary fake repository

---
+++ FinalProjectWebSystems/Models/FakeProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProjectWebSystems.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IEnumerable<Product> Products => new List<Product>
+        {
+            new Product {Name = "Nike Air Force 1", Price = 90},
+            new Product {Name = "Nike Air Max 90", Price = 120},
+            new Product {Name = "Jordan True Flight", Price = 140},
+            new Product {Name = "Adidas NMD R1", Price = 130},
+            new Product {Name = "Nike Air VaporMax Flyknit 3", Price = 200}
+        };
+    }
+}
