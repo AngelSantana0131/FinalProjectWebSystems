e2cff893179008155dcb0452ed795e97f38d7fed
Angel Santana <n01257615@unf.edu>
2021-04-15 18:48:11 -0400

    Created the repository class

---
+++ FinalProjectWebSystems/Models/EFProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProjectWebSystems.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private ApplicationDBContext context;
+        public EFProductRepository(ApplicationDBContext ctx)
+        {
+            context = ctx;
+        }
+        public IEnumerable<Product> Products => context.Products;
+    }
+}
