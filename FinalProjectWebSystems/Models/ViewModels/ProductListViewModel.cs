8b510242b3e9cf94a5e69b1df12110e2eef8bb05
Angel Santana <n01257615@unf.edu>
2021-04-15 21:21:37 -0400

    Updated the list method in product controller

---
+++ FinalProjectWebSystems/Models/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using FinalProjectWebSystems.Models;
+namespace FinalProjectWebSystems.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
