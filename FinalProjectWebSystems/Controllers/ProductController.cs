fe0769c95ab72f1aec9c3b7b3db90efb3c963dc9
Angel Santana <n01257615@unf.edu>
2021-04-21 05:09:39 -0400

    Added more entries per page

--- FinalProjectWebSystems/Controllers/ProductController.cs
+++ FinalProjectWebSystems/Controllers/ProductController.cs
@@ -11,7 +11,7 @@ namespace FinalProjectWebSystems.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
-        public int PageSize = 4;
+        public int PageSize = 6;
         public ProductController(IProductRepository repo)
         {
             repository = repo;
