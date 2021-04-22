b5040ba9602a0a31cf40c93118c852caf5f7b321
Angel Santana <n01257615@unf.edu>
2021-04-22 00:19:20 -0400

    nothing

--- FinalProjectWebSystems/Models/Order.cs
+++ FinalProjectWebSystems/Models/Order.cs
@@ -32,6 +32,7 @@ namespace FinalProjectWebSystems.Models
 
         [Required(ErrorMessage = "Please enter a country name")]
         public string Country { get; set; }
+
         public bool GiftWrap { get; set; }
     }
 }
